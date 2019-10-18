d1096d490f90eaaaf3eecbd2857a34494f2a5879
JULIUS CAEZAR PAIXAO MOISES <a9261602@rn.senac.br>
2019-10-17 21:16:11 -0300

    funfun

---
+++ src/app/contato.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class ContatoService {
+
+  contatoUrl: string;
+
+  private readonly URL_BUSCA_TODOS = `${environment.apiUrl}/contatos-ws`;
+  private readonly URL_SALVA_CONTATO = `${environment.apiUrl}/contatos-ws`;
+
+  constructor(private http: HttpClient ) {}
+
+  buscaTodos(): Promise<any> {
+    return this.http.get(this.URL_BUSCA_TODOS).toPromise();
+  }
+
+salvaContato(contato: any): Promise<any> {
+   return this.http.post(this.URL_SALVA_CONTATO, contato).toPromise();
+}
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://a9262602-passeio.herokuapp.com/'
 };
